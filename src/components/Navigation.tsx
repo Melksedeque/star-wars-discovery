d3eb6cd5b11c87f035e1ea0bcf4bd53db5f55b18
Melksedeque <freelancer@melksedeque.com.br>
2025-03-16 18:39:00 -0300

    Acerto no tamanho dos icones baseado na prop de navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,18 +3,33 @@ import { useLocation } from "react-router-dom";
 import { cn } from "../utils/cn";
 import { useNavigation } from "../contexts/NavigationContext";
 
-export default function Navigation() {
+interface NavigationProps {
+  position?: "left" | "right" | "all";
+  size?: string;
+}
+
+export default function Navigation({
+  position = "all",
+  size = "10",
+}: NavigationProps) {
   const location = useLocation();
   const { items } = useNavigation();
 
+  const displayItems =
+    position === "left"
+      ? items.slice(0, 3)
+      : position === "right"
+      ? items.slice(3, 6)
+      : items;
+
   return (
-    <nav className="flex items-center justify-center gap-20">
-      {items.map((item) => (
+    <nav className="flex items-center justify-center gap-16">
+      {displayItems.map((item) => (
         <Link
           key={item.name}
           to={item.href}
           className={cn(
-            "flex flex-col items-center p-3 rounded-md text-sm font-medium transition-colors",
+            `flex flex-col items-center p-3 rounded-md text-md font-medium transition-colors w-${size}`,
             location.pathname === item.href
               ? "text-yellow-500 bg-yellow-500/10"
               : "text-gray-300 hover:text-yellow-500 hover:bg-yellow-500/10"
