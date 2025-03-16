46956401e7150c53a2ae8a619d656f368a570bca
Melksedeque <freelancer@melksedeque.com.br>
2025-03-16 17:03:29 -0300

    Limpeza de código

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,13 +28,6 @@ export default function Home() {
           alt="Star Wars Logo"
           className="w-64 mb-8"
         />
-        <img
-          src="/images/LogoStarWars.svg"
-          alt="Star Wars Logo"
-          className="w-64 mb-8"
-        />
-        <h1 className="text-4xl font-bold">Welcome to the home page</h1>
-        <p className="text-lg">This is the home page</p>
       </div>
       <nav className="hidden md:flex space-x-8">
         {navigation.map((item) => {
