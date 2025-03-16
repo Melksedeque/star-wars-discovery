baab57346f8ffe317a469b4828efedf780f77ce9
Melksedeque <freelancer@melksedeque.com.br>
2025-03-16 17:12:10 -0300

    Ajuste nos ícones personalizados; Correção no link; Remoção de icones de Lucide React

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,14 @@
 import { Link, useLocation } from "react-router-dom";
 import { cn } from "../utils/cn";
-import {
-  Film,
-  Users,
-  AlignCenter as Alien,
-  Globe,
-  Rocket,
-  Truck,
-} from "lucide-react";
 
 export default function Home() {
   const navigation = [
-    { name: "Films", href: "/", icon: Film },
-    { name: "Characters", href: "/characters", icon: Users },
-    { name: "Species", href: "/species", icon: Alien },
-    { name: "Planets", href: "/planets", icon: Globe },
-    { name: "Starships", href: "/starships", icon: Rocket },
-    { name: "Vehicles", href: "/vehicles", icon: Truck },
+    { name: "Filmes", href: "/", icon: "/icons/films.png" },
+    { name: "Personagens", href: "/characters", icon: "/icons/characters.svg" },
+    { name: "Espécies", href: "/species", icon: "/icons/species.svg" },
+    { name: "Planetas", href: "/planets", icon: "/icons/planets.svg" },
+    { name: "Naves", href: "/starships", icon: "/icons/starships.svg" },
+    { name: "Veículos", href: "/vehicles", icon: "/icons/vehicles.svg" },
   ];
   const location = useLocation();
 
@@ -37,13 +29,13 @@ export default function Home() {
               key={item.name}
               to={item.href}
               className={cn(
-                "flex items-center space-x-2 px-3 py-2 rounded-md text-sm font-medium transition-colors",
+                "flex flex-col items-center space-x-2 px-3 py-2 rounded-md text-sm font-medium transition-colors",
                 location.pathname === item.href
                   ? "text-yellow-500 bg-yellow-500/10"
                   : "text-gray-300 hover:text-yellow-500 hover:bg-yellow-500/10"
               )}
             >
-              <Icon className="w-4 h-4" />
+              <img src={Icon} alt={`Ícone de ${item.name}`} />
               <span>{item.name}</span>
             </Link>
           );
