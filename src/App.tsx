eb7d96d72eb393e55fda41e982d9e0fadf9b66df
Melksedeque <freelancer@melksedeque.com.br>
2025-03-16 15:52:26 -0300

    Construção de rotas das páginas

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Films } from "./pages/Films";
+import { Characters } from "./pages/Characters";
+import { Species } from "./pages/Species";
+import { Planets } from "./pages/Planets";
+import { Starships } from "./pages/Starships.tsx";
+import { Vehicles } from "./pages/Vehicles";
+import Home from "./pages/Home";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/films" element={<Films />} />
+            <Route path="/characters" element={<Characters />} />
+            <Route path="/species" element={<Species />} />
+            <Route path="/planets" element={<Planets />} />
+            <Route path="/starships" element={<Starships />} />
+            <Route path="/vehicles" element={<Vehicles />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
