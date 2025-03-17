a35ac33ba5a8541656550e9f7bce5bb888d032c0
Melksedeque <freelancer@melksedeque.com.br>
2025-03-17 14:16:42 -0300

    Definição de tamanho dos elementos na navegação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
 
   return (
     <header className="bg-black flex items-center justify-center gap-28 p-8 mb-10">
-      {!isHomeRoute && <Navigation position="left" size="16" />}
+      {!isHomeRoute && <Navigation position="left" size="10" />}
       <Link to={"/"}>
         <img
           src="/images/LogoStarWarsDiscovery.png"
@@ -15,7 +15,7 @@ export default function Header() {
           className="w-28"
         />
       </Link>
-      {!isHomeRoute && <Navigation position="right" size="16" />}
+      {!isHomeRoute && <Navigation position="right" size="10" />}
     </header>
   );
 }
