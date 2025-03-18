494d680f7264f78f099416f98176c655a1b9c9b7
Melksedeque <freelancer@melksedeque.com.br>
2025-03-18 17:57:55 -0300

    Remoção de safelist. Sem efeito na aplicação.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  safelist: [
-    'w-10',
-    'w-20',
-  ],
   theme: {
     extend: {},
   },
