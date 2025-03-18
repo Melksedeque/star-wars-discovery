c8c59194f2b3d8173adc8dac6267d9151791b60a
Melksedeque <freelancer@melksedeque.com.br>
2025-03-18 17:57:12 -0300

    Criação de classes em safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    'w-10',
+    'w-20',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
