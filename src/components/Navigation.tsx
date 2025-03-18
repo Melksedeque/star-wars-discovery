25ae92a928a63d3660738ed0f1b0522915bb9760
Melksedeque <freelancer@melksedeque.com.br>
2025-03-18 17:57:30 -0300

    Ajuste na chamada de classe interpolada dentro do cn

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -29,7 +29,8 @@ export default function Navigation({
           key={item.name}
           to={item.href}
           className={cn(
-            `flex flex-col w-${size} items-center p-3 text-md text-white font-bold transition-all duration-300 hover:scale-120`,
+            `flex flex-col items-center p-3 text-md text-white font-bold transition-all duration-300 hover:scale-120`,
+            `w-${size}`,
             location.pathname === item.href ? "scale-120" : ""
           )}
         >
